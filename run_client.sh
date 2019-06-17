7e62eb39090d7a3855c0ceb361d745dfedd02bcc
ubuntu <ubuntu@localhost.localdomain>
2019-06-17 11:51:20 +0800

    update

---
+++ run_client.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo > ngrok.cfg
+echo server_addr: "ngrok.evink.cn:4443"  >> ngrok.cfg
+echo trust_host_root_certs: false >> ngrok.cfg
+/usr/local/ngrok/bin/ngrokd -config=ngrok.cfg -subdomain=api "$1"
